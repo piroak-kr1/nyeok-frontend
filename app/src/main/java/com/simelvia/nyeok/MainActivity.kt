75874e0c402eeae6c6ac247008ab5891a8a19a71
kimkun07 <kimkun07@naver.com>
2024-07-01 16:19:10 +0900

    Initial commit

---
+++ app/src/main/java/com/simelvia/nyeok/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.simelvia.nyeok
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.simelvia.nyeok.ui.theme.NyeokTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NyeokTheme { // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                ) {
+                    EchoTest()
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun EchoTest(modifier: Modifier = Modifier) {
+    Column {
+        Row {
+            TextField(value = "xx", onValueChange = { /*TODO*/ })
+            Button(onClick = { /*TODO*/ }) {}
+        }
+        Text("Server response: ")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun EchoTestPreview() {
+    NyeokTheme {
+        EchoTest()
+    }
+}
